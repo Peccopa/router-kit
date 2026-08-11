32552c9cd0f8d002b6ae3d66d7a9593d5308db3d
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-08-11 08:46:40 +0300

    feat: add navigation function

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,17 @@ function getRoute(path: string): string {
   return routes[path] || 'Home';
 }
 
+function navigate(path: string): void {
+  history.pushState({}, '', path);
+
+  console.log(getRoute(window.location.pathname));
+}
+
 document.addEventListener('click', (event) => {
   event.preventDefault();
 
   if (!(event.target instanceof HTMLAnchorElement)) return;
 
-  const url = new URL(event.target.href);
-
-  history.pushState({}, '', url.pathname);
-
-  console.log(getRoute(window.location.pathname));
+  const path = event.target.pathname;
+  navigate(path);
 });
-
-const path = window.location.pathname;
-
-console.log(getRoute(path));
